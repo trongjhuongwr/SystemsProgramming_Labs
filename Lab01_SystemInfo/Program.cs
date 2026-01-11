0c9f7dbd07eaa35e87152c4d226312406551189d
trongjhuongwr <nguyentronghuong05@gmail.com>
2026-01-11 15:30:46 +0700

    Initial commit: Lab 1 System Info completed

--- Lab01_SystemInfo/Program.cs
+++ Lab01_SystemInfo/Program.cs
@@ -18,3 +18,11 @@ Console.WriteLine($"[3] System Time: {systemTime}");
 Console.WriteLine("==================================");
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
+
+/* Output:
+=== SYSTEM INFORMATION PROGRAM ===
+[1] OS Version: Microsoft Windows NT 10.0.26100.0
+[2] Current Directory: H:\chuong_trinh_hoc_UEH\mon_hoc_ki_6\systems_programming\systems_programming_labs\SystemsProgramming_Labs\Lab01_SystemInfo\bin\Debug\net8.0
+[3] System Time: 1/11/2026 15:28:06
+==================================
+*/
